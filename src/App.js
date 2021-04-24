679bd73457d014f5c8ae46cb9779b8142731ebd1
avipbstrd <avipbstrd>
2021-04-24 23:36:28 +0700

    diedit_di_App.Js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          testing avip
+          testing 12345
         </p>
         <a
           className="App-link"
