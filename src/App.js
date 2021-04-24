e609517692f6bf5c3dd903e1fc34935fea73007e
aldyivans <ivanbrutalart@gmail.com>
2021-04-24 23:47:04 +0700

    tes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          avip
+          DasASasAS
         </p>
         <a
           className="App-link"
