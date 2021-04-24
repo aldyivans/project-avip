30cebee67196f1a2a7837d46e2b76e2a63b4255c
avipbstrd <avipbstrd>
2021-04-24 23:40:21 +0700

    edit Css dan App

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          testing 12345
+          testing 
         </p>
         <a
           className="App-link"
